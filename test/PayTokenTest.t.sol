52697b1565b8b52bfca8fda090b6742a7dddb3fa
Samuel Parke <samueljparke@gmail.com>
2025-07-23 14:51:01 +0100

    successful tests for pay token

--- test/PayTokenTest.t.sol
+++ test/PayTokenTest.t.sol
@@ -10,4 +10,29 @@ contract PayTokenTest is Test {
     function setUp() public {
         token = new PayToken();
     }
+
+    function testBurnBalanceMustBeMoreThanAmountRevert() public {
+        vm.expectRevert(PayToken.PayToken__BalanceMustBeMoreThanBurnAmount.selector);
+        token.burn(1 ether);
+    }
+
+    function testMintAndThenBurnAmount() public {
+        token.mint(address(this), 1 ether);
+        uint256 balanceAfterMint = token.balanceOf(address(this));
+        token.burn(1 ether);
+        uint256 balanceAfterBurn = token.balanceOf(address(this));
+        assertEq(balanceAfterMint, 1 ether);
+        assertEq(balanceAfterBurn, 0);
+    }
+
+    function testMustbeMoreThanZeroMintRevert() public {
+        vm.expectRevert(PayToken.PayToken__MustBeMoreThanZero.selector);
+        token.mint(address(this), 0);
+    }
+
+    function testMustbeMoreThanZeroBurnRevert() public {
+        token.mint(address(this), 1 ether);
+        vm.expectRevert(PayToken.PayToken__MustBeMoreThanZero.selector);
+        token.burn(0);
+    }
 }
