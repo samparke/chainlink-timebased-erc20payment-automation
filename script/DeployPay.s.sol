58c69495c8c0b971f6313732e9deccf69227e4a1
Samuel Parke <samueljparke@gmail.com>
2025-07-28 08:21:29 +0100

    anvil deployer key only

--- script/DeployPay.s.sol
+++ script/DeployPay.s.sol
@@ -7,13 +7,8 @@ import {PayToken} from "../src/PayToken.sol";
 
 contract DeployPay is Script {
     function run() external returns (PayToken, Payment) {
-        uint256 deployerKey;
-        if (block.chainid == 31337) {
-            // anvil
-            deployerKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
-        } else {
-            deployerKey = vm.envUint("PRIVATE_KEY");
-        }
+        // this is anvil key, not personal
+        uint256 deployerKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
         vm.startBroadcast(deployerKey);
         PayToken payToken = new PayToken();
         Payment paymentContract = new Payment(address(payToken));
